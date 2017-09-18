23e6db977169639822de89f9f81f18d93c2921e8
maumg1196 <maumg1196@gmail.com>
2017-09-17 22:39:29 -0500

    Add Secant Method

---
+++ secant.py
@@ -0,0 +1,16 @@
+"""Secant Method."""
+from sympy import *
+
+
+def secant(f, x0, x1, iteration):
+    """Here is all the code of the method."""
+    for cycle in range(iteration):
+        x2 = x1 - f(x1) * ((x1 - x0) / (f(x1) - f(x0)))
+        x0 = x1
+        x1 = x2
+    return x2
+
+
+def function(x):
+    """Here you gonna code the function you want to evaluate."""
+    pass
